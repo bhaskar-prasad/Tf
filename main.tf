d14bd1cd82e2bd009c47955a002ca8de23a9bfb5
bhaskar-prasad <bhaskar.prasad@triconinfotech.com>
2024-11-19 11:18:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
+
 
 resource "aws_instance" "example" {
   ami           = var.ami_id
